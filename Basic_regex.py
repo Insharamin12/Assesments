1090e827faea275c2d6a97d35d330e995a36ddc2
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-02-07 15:09:36 +0530

    Update Basic_regex.py
    Added code

--- Basic_regex.py
+++ Basic_regex.py
@@ -1,3 +1,4 @@
+#Question 1
 #The check_web_address function checks if the text passed qualifies as a top-level web address,
 #meaning that it contains alphanumeric characters (which includes letters, numbers, and underscores),
 #as well as periods, dashes, and a plus sign,
@@ -38,6 +39,25 @@ print(check_time("five o'clock")) # False
 #with at least the first character in uppercase (if it's a letter), returning True if the condition is met, or False otherwise.
 #For example, "Instant messaging (IM) is a set of communication technologies used for text-based communication"
 #should return True since (IM) satisfies the match conditions." Fill in the regular expression in this function: 
+
+import re
+def contains_acronym(text):
+  pattern = r"\([A-Z0-9][A-Za-z0-9]+\)" 
+  result = re.search(pattern, text)
+  return result != None
+
+print(contains_acronym("Instant messaging (IM) is a set of communication technologies used for text-based communication")) # True
+print(contains_acronym("American Standard Code for Information Interchange (ASCII) is a character encoding standard for electronic communication")) # True
+print(contains_acronym("Please do NOT enter without permission!")) # False
+print(contains_acronym("PostScript is a fourth-generation programming language (4GL)")) # True
+print(contains_acronym("Have fun using a self-contained underwater breathing apparatus (Scuba)!")) # True
+
+
+
+
+
+
+
 
 
 
