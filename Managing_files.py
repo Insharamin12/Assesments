a6b4ce2a4e7dbea2d7953c521c9c58ff7af7b231
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-02-07 11:55:12 +0530

    Update Managing_files.py

--- Managing_files.py
+++ Managing_files.py
@@ -10,3 +10,15 @@
 #4.Add each guest's name in the Python list to the file one by one.
 #The current names in the guests.txt file should be: Bob, Polly, Sam, Danielle and Jacob.
 #Now let's check whether Bob and Andrea are still checked in
+
+guests = open("guests.txt", "w")
+initial_guests = ["Bob", "Andrea", "Manuel", "Polly", "Khalid"]
+
+for i in initial_guests:
+    guests.write(i + "\n")
+    
+guests.close()
+
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
